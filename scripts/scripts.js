8527907353a29c94c2b45007961395c37b510b4d
Chris Millar <chris@millr.org>
2024-02-06 13:27:46 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -303,6 +303,17 @@ document.body.addEventListener('keydown', showCookieBanner, { once: true, passiv
 
 loadPage();
 
+// Side-effects
+(async function daPreview() {
+  const { searchParams } = new URL(window.location.href);
+  const daPreview = searchParams.get('dapreview');
+  if (daPreview) {
+    const origin = daPreview === 'local' ? 'http://localhost:3000' : 'https://da.live';
+    const { default: livePreview } = await import(`${origin}/scripts/dapreview.js`);
+    livePreview(loadPage);
+  }
+}());
+
 const previewed = ({ detail }) => {
   const sk = detail.data;
   // your custom code from button.action goes here
